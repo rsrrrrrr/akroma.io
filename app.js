adb4ee82bb676e2e02658a549409315d09e82a4d
Eric Polerecky <eric@polerecky.com>
2018-01-06 10:34:34 -0500

    scaffolding

---
+++ app.js
@@ -0,0 +1,81 @@
+
+
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes/index');
+const users = require('./routes/user');
+
+const app = express();
+
+const env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            title: 'error'
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
+    });
+});
+
+// app.set('port', process.env.PORT || 3005);
+// var http = require('http').Server(app);
+// //var io = require('socket.io')(http);
+
+// // web3socket(io);
+
+// http.listen(app.get('port'), '0.0.0.0', function() {
+//     console.log('Express server listening on port ' + app.get('port'));
+// });
+
+
+module.exports = app;
---
+++ public/js/site.js
@@ -0,0 +1,19 @@
+/* activate scrollspy menu */
+$('body').scrollspy({
+    target: '#nav',
+    offset: 150
+});
+
+/* animate smooth scrolling sections */
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top - 50
+            }, 500);
+            return false;
+        }
+    }
+});
