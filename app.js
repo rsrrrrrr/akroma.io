590805946e1434a052d5031c07175fa798921f3f
Eric Polerecky <eric@polerecky.com>
2018-01-17 16:14:07 -0500

    testing webhooks

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
 // view engine setup
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
